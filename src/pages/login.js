03c09d81a2217d917cb2dceba9506d4fcd876ff0
Dana Scheider <dana.scheider@envato.com>
2021-06-18 17:43:19 +1000

    Start troubleshooting login issue

--- src/pages/login.js
+++ src/pages/login.js
@@ -14,7 +14,10 @@ import styles from './login.module.css'
 const LoginPage = () => {
   const [cookies, setCookie, removeCookie] = useCookies([sessionCookieName])
 
-  const successCallback = ({ tokenId }) => {
+  const successCallback = (resp) => {
+    const { tokenId } = resp
+
+    console.log(resp)
     const backendUri = `${backendBaseUri[process.env.NODE_ENV]}/auth/verify_token`
 
     if (cookies[sessionCookieName] !== tokenId) {
