8f9eb4647302ec8d481017bd0af64023cfd65922
Dana Scheider <dana.scheider@envato.com>
2021-06-18 17:28:10 +1000

    Make main dashboard page work

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,7 +5,46 @@ import paths from '../routing/paths'
 import { backendBaseUri, sessionCookieName } from '../utils/config'
 import ReactLoading from 'react-loading'
 import DashboardHeader from '../components/dashboardHeader/dashboardHeader'
+import NavigationMosaic from '../components/navigationMosaic/navigationMosaic'
 import styles from './dashboard.module.css'
+
+const cards = [
+  {
+    backgroundColor: '#FFBF00',
+    textColor: '#000000',
+    href: paths.shoppingLists,
+    children: 'Your Shopping Lists',
+    key: 'shopping-lists'
+  },
+  {
+    backgroundColor: '#E83F6F',
+    textColor: '#FFFFFF',
+    href: '#',
+    children: 'Nav Link 2',
+    key: 'nav-link-2'
+  },
+  {
+    backgroundColor: '#2274A5',
+    textColor: '#FFFFFF',
+    href: '#',
+    children: 'Nav Link 3',
+    key: 'nav-link-3'
+  },
+  {
+    backgroundColor: '#00A323',
+    textColor: '#FFFFFF',
+    href: '#',
+    children: 'Nav Link 4',
+    key: 'nav-link-4'
+  },
+  {
+    backgroundColor: '#20E2E9',
+    textColor: '#000000',
+    href: '#',
+    children: 'Nav Link 5',
+    key: 'nav-link-5'
+  }
+]
 
 const DashboardPage = () => {
   const [cookies, , removeCookie] = useCookies([sessionCookieName])
@@ -39,9 +78,10 @@ const DashboardPage = () => {
 
   return(!!shouldRedirect ?
     <Redirect to={paths.login} /> :
-    <div className={styles.root}>
+    <>
       {!!userData ? <DashboardHeader data={userData} /> : <ReactLoading type='spinningBubbles' color='#ccc' />}
-    </div>
+      <div className={styles.body}><NavigationMosaic cardArray={cards} /></div>
+    </>
   )
 }
 
