bb9559d4c2025b40a03294911adff4152870d06b
Dana Scheider <dana.scheider@envato.com>
2021-06-19 08:49:45 +1000

    Rip out logout functionality

--- src/components/dashboardHeader/dashboardHeader.js
+++ src/components/dashboardHeader/dashboardHeader.js
@@ -1,23 +1,9 @@
-import React, { useState } from 'react'
-import { Redirect } from 'react-router-dom'
-import { useCookies } from 'react-cookie'
-import { sessionCookieName } from '../../utils/config'
-import LogoutDropdown from '../logoutDropdown/logoutDropdown'
+import React from 'react'
 import anonymousAvatar from './anonymousAvatar.jpg'
 import styles from './dashboardHeader.module.css'
-import paths from '../../routing/paths'
 
 const DashboardHeader = ({ data }) => {
   const { email, name, image_url } = data
-
-  const [dropdownVisible, setDropdownVisible] = useState(false)
-  const [, , removeCookie] = useCookies([sessionCookieName])
-
-  const logoutSuccess = () => {
-    removeCookie(sessionCookieName)
-    return <Redirect to={paths.home} />
-  }
-  const logoutFailure = () => console.log('Failed to log out')
 
   return(
     <div className={styles.root}>
@@ -25,18 +11,13 @@ const DashboardHeader = ({ data }) => {
         <span className={styles.headerContainer}>
           <h1 className={styles.header}>Skyrim Inventory Management</h1>
         </span>
-        <button className={styles.profile} onClick={() => setDropdownVisible(!dropdownVisible)}>
+        <span className={styles.profile}>
           <div className={styles.profileText}>
             <p className={styles.textTop}>{name}</p>
             <p className={styles.textBottom}>{email}</p>
           </div>
           <img className={styles.avatar} src={image_url || anonymousAvatar} alt='User avatar' referrerPolicy='no-referrer' />
-        </button>
-          <LogoutDropdown
-            className={dropdownVisible ? styles.logoutDropdown : styles.hidden}
-            successCallback={logoutSuccess}
-            failureCallback={logoutFailure}
-          />
+        </span>
       </div>
     </div>
   )
--- src/components/logoutDropdown/logoutDropdown.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LogoutDropdown from './logoutDropdown'
-
-const noop = () => {}
-
-export default { title: 'LogoutDropdown' }
-
-export const Default = () => <LogoutDropdown successCallback={noop} failureCallback={noop} />
