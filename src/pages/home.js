6c92d5247bb6c53ea454b4deb2025ac47f9dade1
Dana Scheider <dana.scheider@envato.com>
2021-06-18 21:37:40 +1000

    Redirect to dashboard if user logged in

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
+import { sessionCookieName } from '../utils/config'
 import paths from '../routing/paths'
 import styles from './home.module.css'
 
 const HomePage = () => {
-  return(
+  const [cookies, , ,] = useCookies([sessionCookieName])
+
+  return(!!cookies[sessionCookieName] ?
+    <Redirect to={paths.dashboard.main} /> :
     <div className={styles.root}>
       <div className={styles.container}>
         <h1 className={styles.header}>Skyrim Inventory Management</h1>
