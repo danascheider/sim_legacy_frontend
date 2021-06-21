953235dc35a62ef95a0b2535e27df6213aae8370
Dana Scheider <dana.scheider@envato.com>
2021-06-21 19:34:42 +1000

    Remove unneeded return statement

--- src/components/logoutDropdown/logoutDropdown.js
+++ src/components/logoutDropdown/logoutDropdown.js
@@ -2,20 +2,17 @@ import React from 'react'
 import icon from './googleIcon.svg'
 import styles from './logoutDropdown.module.css'
 
-const LogoutDropdown = ({className, logOutUser}) => {
-
-  return(
-    <div className={className}>
-      <button className={styles.button} onClick={logOutUser}>
-        <div className={styles.body}>
-          <div className={styles.googleLogout}>
-            <img src={icon} alt='Google logo' />
-            Log Out With Google
-          </div>
+const LogoutDropdown = ({className, logOutUser}) => (
+  <div className={className}>
+    <button className={styles.button} onClick={logOutUser}>
+      <div className={styles.body}>
+        <div className={styles.googleLogout}>
+          <img src={icon} alt='Google logo' />
+          Log Out With Google
         </div>
-      </button>
-    </div>
-  )
-}
+      </div>
+    </button>
+  </div>
+)
 
 export default LogoutDropdown
