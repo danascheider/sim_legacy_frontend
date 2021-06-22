7d2c3648cf923892d4ee86b42a4c22668a5cd21d
Dana Scheider <dana.scheider@envato.com>
2021-06-22 13:18:56 +1000

    WIP on styles for shopping list item element

--- src/components/shoppingListItem/shoppingListItem.js
+++ src/components/shoppingListItem/shoppingListItem.js
@@ -1,26 +1,42 @@
 import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 import SlideToggle from 'react-slide-toggle'
 import styles from './shoppingListItem.module.css'
 
-const ShoppingListItem = ({ description, quantity, notes }) => {
+const ShoppingListItem = ({
+  description,
+  quantity,
+  notes,
+  colorScheme
+}) => {
+  const { schemeColor, textColor, borderColor } = colorScheme
+
   const [toggleEvent, setToggleEvent] = useState(0)
 
   const toggleDetails = () => {
     setToggleEvent(Date.now)
   }
 
+  const styleVars = {
+    '--background-color': schemeColor,
+    '--text-color': textColor,
+    '--border-color': borderColor
+  }
+
   return(
-    <div className={styles.root}>
-      <button className={styles.titleBar} onClick={toggleDetails}>
-        <span><h3 className={styles.description}>{description}</h3></span>
-        <span className={styles.quantity}>{quantity}</span>
-      </button>
+    <div className={styles.root} style={styleVars}>
+      <div className={classnames(styles.headerBar, styles.flexItem)}>
+        <button className={styles.button} onClick={toggleDetails}>
+          <h3 className={styles.description}>{description}</h3>
+        </button>
+        <span className={classnames(styles.quantity, styles.flexItem)}>{quantity}</span>
+      </div>
       <SlideToggle toggleEvent={toggleEvent} collapsed>
         {({ setCollapsibleElement }) => (
           <div className={styles.collapsible}>
             <div className={styles.collapsibleContent} ref={setCollapsibleElement}>
-              {notes}
+              {notes || 'No details available'}
             </div>
           </div>
         )}
@@ -30,6 +46,8 @@ const ShoppingListItem = ({ description, quantity, notes }) => {
 }
 
 ShoppingListItem.propTypes = {
+  className: PropTypes.string.isRequired,
+  colorScheme: PropTypes.shape.isRequired,
   description: PropTypes.string.isRequired,
   quantity: PropTypes.number.isRequired,
   notes: PropTypes.string
--- src/components/shoppingListItem/shoppingListItem.stories.js
+++ src/components/shoppingListItem/shoppingListItem.stories.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ShoppingListItem from './shoppingListItem'
 
+const colorScheme = {
+  schemeColor: '#E83F6F',
+  textColor: '#FFFFFF',
+  borderColor: '#B93258'
+}
+
 export default { title: 'ShoppingListItem' }
 
 export const Default = () => (
@@ -8,5 +14,6 @@ export const Default = () => (
     description='Ebony sword'
     quantity={2}
     notes='One to enchant with Soul Trap, one to enchant with Absorb Health'
+    colorScheme={colorScheme}
   />
 )
