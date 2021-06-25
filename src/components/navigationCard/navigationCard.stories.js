381df6b40d7835bb675ad1d0bf96b46a799e14ef
Dana Scheider <dana.scheider@envato.com>
2021-06-25 16:10:26 +1000

    Incorporate ColorContext into NavigationCard/NavigationMosaic components

--- src/components/navigationCard/navigationCard.stories.js
+++ src/components/navigationCard/navigationCard.stories.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { GREEN } from '../../utils/colorSchemes'
+import { ColorProvider } from '../../contexts/colorContext'
 import NavigationCard from './navigationCard'
 
 export default { title: 'NavigationCard' }
 
 export const Default = () => (
-  <NavigationCard colorScheme={GREEN} href='#'>
-    Your Shopping List
-  </NavigationCard>
+  <ColorProvider colorScheme={GREEN}>
+    <NavigationCard colorScheme={GREEN} href='#'>
+      Your Shopping List
+    </NavigationCard>
+  </ColorProvider>
 )
