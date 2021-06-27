fc6443c60503e747d34f9da95eb414a17d4bf9e0
Dana Scheider <dana.scheider@envato.com>
2021-06-27 21:13:54 +1000

    Use simApi abstraction to fetch user profile data

--- src/utils/simApi.js
+++ src/utils/simApi.js
@@ -12,3 +12,50 @@ export const authorize = token => {
     fetch(uri, {  headers: authHeader(token) })
   )
 }
+
+/*
+ *
+ * User Profile Endpoint
+ * 
+ */
+
+export const fetchUserProfile = token => {
+  const uri = `${baseUri}/users/current`
+
+  return(
+    fetch(uri, { headers: authHeader(token) })
+  )
+}
+
+/*
+ *
+ * Shopping List Endpoints (Scoped to Authenticated User)
+ *
+ */
+
+// GET index
+export const fetchShoppingLists = token => {
+  const uri = `${baseUri}/shopping_lists`
+
+  return(
+    fetch(uri, { headers: authHeader(token) })
+  )
+}
+
+// PATCH update
+export const updateShoppingList = (token, listId, attrs) => {
+  const uri = `${baseUri}/shopping_lists/${listId}`
+
+  const body = JSON.stringify({
+    id: listId,
+    shopping_list: attrs
+  })
+
+  return(
+    fetch(uri, {
+      method: 'PATCH',
+      headers: combinedHeaders(token),
+      body: body
+    })
+  )
+}
