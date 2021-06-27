0bacb6f25b84c70a5d5d69dccde835fd805df0b2
Dana Scheider <dana.scheider@envato.com>
2021-06-27 13:45:33 +1000

    Finish (I hope) designing DashboardContext and DashboardProvider

--- src/contexts/dashboardContext.js
+++ src/contexts/dashboardContext.js
@@ -1,7 +1,30 @@
 import { createContext, useState } from 'react'
 import { useCookies } from 'react-cookie'
 import PropTypes from 'prop-types'
+import { sessionCookieName } from '../utils/config'
 
 const DashboardContext = createContext()
 
-export { DashboardContext }
+const DashboardProvider = ({ children }) => {
+  const [cookies, setCookie, removeCookie] = useCookies([sessionCookieName])
+  const [profileData, setProfileData] = useState(null)
+
+  const setSessionCookie = val => setCookie(sessionCookieName, val)
+  const removeSessionCookie = () => removeCookie(sessionCookieName)
+
+  const value = {
+    token: cookies[sessionCookieName],
+    profileData,
+    setSessionCookie,
+    removeSessionCookie,
+    setProfileData
+  }
+
+  return(
+    <DashboardContext.Provider value={value}>
+      {children}
+    </DashboardContext.Provider>
+  )
+}
+
+export { DashboardContext, DashboardProvider }
