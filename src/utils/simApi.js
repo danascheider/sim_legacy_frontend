b4279cd5ab7188efdb9d89ab4fc9c9828023518a
Dana Scheider <dana.scheider@envato.com>
2021-06-28 19:15:32 +1000

    Remove attempt at custom error constructor because it turns out that's kind of hard when you want to use a function and not a class

--- src/utils/simApi.js
+++ src/utils/simApi.js
@@ -1,13 +1,13 @@
 import { backendBaseUri } from './config'
-import { AuthorizationError } from './customErrors'
 
+const authError = (msg = '401 Unauthorized') => ({ name: 'AuthorizationError', code: 401, message: msg })
 const baseUri = backendBaseUri[process.env.NODE_ENV]
 const authHeader = token => ({ 'Authorization': `Bearer ${token}`})
 const contentTypeHeader = { 'Content-Type': 'application/json' }
 const combinedHeaders = token => ({ ...authHeader(token), ...contentTypeHeader })
 
 const throwAuthErrorOn401 = resp => {
-  if (resp.status === 401) resp.json().then(data => { throw new AuthorizationError(data.error) })
+  if (resp.status === 401) resp.json().then(data => { throw authError() })
 }
 
 /*
