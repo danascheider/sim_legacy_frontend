82e60dde39f974290ea3e6d67295104745d2b112
Dana Scheider <dana.scheider@envato.com>
2021-06-30 14:45:23 +1000

    Revert "Remove unused ref" I removed the ref but it turned out it actually was used. This reverts commit affb8000b9d08108afa47f5c797aab7ee53a652e.

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { Redirect } from 'react-router-dom'
 import GoogleLogin from 'react-google-login'
 import { useCookies } from 'react-cookie'
@@ -16,6 +16,8 @@ import logOutWithGoogle from '../../utils/logOutWithGoogle'
 const LoginPage = () => {
   const [cookies, setCookie, removeCookie] = useCookies([sessionCookieName])
   const [loginErrorMessage, setLoginErrorMessage] = useState(null)
+
+  const mountedRef = useRef(true)
 
   const successCallback = (resp) => {
     const { tokenId } = resp
@@ -43,6 +45,10 @@ const LoginPage = () => {
     cookies[sessionCookieName] && removeCookie(sessionCookieName)
     setLoginErrorMessage('Something went wrong! Please try logging in again.')
   }
+
+  useEffect(() => (
+    () => (mountedRef.current = false)
+  ))
 
   return(cookies[sessionCookieName] && !isStorybook() ?
     <Redirect to={paths.dashboard.main} /> :
