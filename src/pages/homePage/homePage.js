9e2078ac3950692c5cfa271034622d4199e86d62
Dana Scheider <dana.scheider@envato.com>
2021-07-02 08:12:59 +1000

    Handle 401 error on homepage

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { authorize } from '../../utils/simApi'
 import isStorybook from '../../utils/isStorybook'
+import logOutWithGoogle from '../../utils/logOutWithGoogle'
 import { useAppContext } from '../../hooks/contexts'
 import paths from '../../routing/paths'
 import styles from './homePage.module.css'
@@ -13,12 +14,12 @@ const HomePage = () => {
     if (token && !isStorybook()) {
       authorize(token)
         .then((resp) => {
-          if (resp.status === 401) {
-            removeSessionCookie() // it's expired
-            return null
-          } else {
-            setShouldRedirectTo(paths.dashboard.main)
-          }
+          setShouldRedirectTo(paths.dashboard.main)
+        })
+        .catch(err => {
+          logOutWithGoogle(() => {
+            removeSessionCookie()
+          })
         })
     }
   }
