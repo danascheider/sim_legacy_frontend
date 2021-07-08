72ecdd0119bcebb67afbb992e37b526c719e842e
Dana Scheider <dana.scheider@envato.com>
2021-07-09 07:15:58 +1000

    Add mock API calls to make sure increment/decrement buttons work

---
+++ src/components/shoppingListItem/storyData.js
@@ -0,0 +1,32 @@
+export const shoppingLists = [
+  {
+    id: 1,
+    user_id: 24,
+    master: true,
+    title: 'Master',
+    list_items: [
+      {
+        id: 2,
+        list_id: 2,
+        description: 'Ebony sword',
+        quantity: 4,
+        notes: 'notes 1'
+      }
+    ]
+  },
+  {
+    id: 2,
+    user_id: 24,
+    master: false,
+    title: 'Heljarchen Hall',
+    list_items: [
+      {
+        id: 1,
+        list_id: 1,
+        description: 'Ebony sword',
+        quantity: 4,
+        notes: 'notes 1'
+      }
+    ]
+  }
+]
