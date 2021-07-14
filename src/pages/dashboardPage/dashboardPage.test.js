45b274b0fd790ec9e530e49013ac829943c81226
Dana Scheider <dana.scheider@envato.com>
2021-07-14 12:46:15 +1000

    Initial tests for the dashboard page - testing that it redirects if the user is not logged in or their token is invalid

---
+++ src/pages/dashboardPage/dashboardPage.test.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { useCookies } from 'react-cookie'
+import { waitFor, screen, fireEvent } from '@testing-library/react'
+import { renderWithRouter } from '../../setupTests'
+import { sessionCookieName } from '../../utils/config'
+import { AppProvider } from '../../contexts/appContext'
+import DashboardPage from './dashboardPage'
+
+describe('DashboardPage', () => {
+  describe('when the user is not signed in', () => {
+    const token = null
+
+    beforeEach(() => {
+      jest.mock('react-cookie', () => ({
+        __esModule: true,
+        useCookies: jest.fn(() => [{ '_sim_google_session': null }, () => {}, () => {}])
+      }))
+    })
+
+    it('redirects to the login page', async () => {
+      const { history } = renderWithRouter(<AppProvider overrideValue={{ token: null }}><DashboardPage /></AppProvider>, { route: '/dashboard' })
+
+      await waitFor(() => expect(history.location.pathname).toEqual('/login'))
+    })
+  })
+
+  describe('when the user token is expired or invalid', () => {
+    const server = setupServer(
+      rest.get('http://localhost:3000/users/current', (req, res, ctx) => {
+        return res(
+          ctx.status(401),
+          ctx.json({ errors: ['Google OAuth token validation error'] })
+        )
+      })
+    )
+
+    beforeAll(() => server.listen())
+    beforeEach(() => {
+      jest.mock('react-cookie', () => ({
+        __esModule: true,
+        useCookies: jest.fn(() => [{ '_sim_google_session': 'xxxxxxx' }, () => {}, () => {}])
+      }))
+      server.resetHandlers()
+    })
+    afterAll(() => server.close())
+
+    it('redirects to the login page', async () => {
+      const { history } = renderWithRouter(<AppProvider><DashboardPage /></AppProvider>, { route: '/dashboard' })
+
+      await waitFor(() => expect(history.location.pathname).toEqual('/login'))
+    })
+  })
+})
