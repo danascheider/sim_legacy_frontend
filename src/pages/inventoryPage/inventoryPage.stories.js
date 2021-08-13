8c2ab07ebdcfabd7c8a4d3be0c1fddd3af37ac8e
Dana Scheider <dana.scheider@envato.com>
2021-08-14 08:49:07 +1000

    Fix some styling and flesh out stories for inventory lists page

--- src/pages/inventoryPage/inventoryPage.stories.js
+++ src/pages/inventoryPage/inventoryPage.stories.js
@@ -8,12 +8,24 @@ import { InventoryListsProvider } from '../../contexts/inventoryListsContext'
 import {
   token,
   profileData,
+  emptyGames,
   games,
+  emptyInventoryLists,
   allInventoryLists
 } from '../../sharedTestData'
 import InventoryPage from './inventoryPage'
 
 export default { title: 'InventoryPage' }
+
+/* 
+ * 
+ * When the user is logged in, has inventory lists, and the inventory
+ * lists are able to be created or updated without incident. Also tests that,
+ * when a user assigns a blank title and the API response returns a default
+ * one, the title first stays blank then updates to the default one when the
+ * response comes back.
+ * 
+ */
 
 export const HappyPath = () => (
   <AppProvider overrideValue={{ profileData, token }}>
@@ -56,6 +68,183 @@ HappyPath.parameters = {
           ctx.status(404)
         )
       }
+    })
+  ]
+}
+
+/*
+ *
+ * When the user has no games
+ *
+ */
+
+export const NoGames = () => {
+  return(
+    <AppProvider overrideValue={{ profileData, token }}>
+      <GamesProvider overrideValue={{ games: emptyGames, gameLoadingState: 'done' }}>
+        <InventoryListsProvider>
+          <InventoryPage />
+        </InventoryListsProvider>
+      </GamesProvider>
+    </AppProvider>
+  )
+}
+
+
+// This is the only story where I'll define handlers for creating a game and
+// fetching its (empty) shopping lists, since it's easy to just say all requests
+// for shopping lists for this story will return an empty array.
+NoGames.parameters = {
+  msw: [
+    rest.post(`${backendBaseUri}/games`, (req, res, ctx) => {
+      const name = req.body.game.name
+
+      return res(
+        ctx.status(201),
+        ctx.json({
+          id: Math.floor(Math.random() * 10000),
+          user_id: profileData.id,
+          description: null,
+          name
+        })
+      )
+    }),
+    rest.get(`${backendBaseUri}/games/:gameId/inventory_lists`, (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json(emptyInventoryLists)
+      )
+    })
+  ]
+}
+
+/*
+ *
+ * When the requested game doesn't exist or doesn't belong to
+ * the authenticated user
+ *
+ */
+
+export const GameNotFoundOnLoad = () => (
+  <AppProvider overrideValue={{ profileData, token }}>
+    <GamesProvider overrideValue={{ games }}>
+      <InventoryListsProvider>
+        <InventoryPage />
+      </InventoryListsProvider>
+    </GamesProvider>
+  </AppProvider>
+)
+
+GameNotFoundOnLoad.parameters = {
+  msw: [
+    rest.get(`${backendBaseUri}/games/:gameId/inventory_lists`, (req, res, ctx) => {
+      return res(
+        ctx.status(404)
+      )
+    })
+  ]
+}
+
+/*
+ *
+ * When the requested game has loaded and then been deleted on
+ * another device or browser, attempting to create a shopping list
+ * for that game will result in a 404. This is an edge case.
+ *
+ */
+
+export const GameNotFoundOnCreate = () => (
+  <AppProvider overrideValue={{ profileData, token }}>
+    <GamesProvider overrideValue={{ games, gameLoadingState: 'done' }}>
+      <InventoryListsProvider>
+        <InventoryPage />
+      </InventoryListsProvider>
+    </GamesProvider>
+  </AppProvider>
+)
+
+GameNotFoundOnCreate.parameters = {
+  msw: [
+    rest.get(`${backendBaseUri}/games/:gameId/inventory_lists`, (req, res, ctx) => {
+      const gameId = parseInt(req.params.gameId)
+      const lists = allInventoryLists.filter(list => list.game_id === gameId)
+
+      return res(
+        ctx.status(200),
+        ctx.json(lists)
+      )
+    }),
+    rest.post(`${backendBaseUri}/games/:gameId/inventory_lists`, (req, res, ctx) => {
+      return res(
+        ctx.status(404)
+      )
+    }),
+    rest.patch(`${backendBaseUri}/inventory_lists/:id`, (req, res, ctx) => {
+      return res(
+        ctx.status(404)
+      )
+    })
+  ]
+}
+
+/*
+ *
+ * When the game has no inventory lists
+ * 
+ */
+
+export const NoLists = () => (
+  <AppProvider overrideValue={{ profileData, token }}>
+    <GamesProvider overrideValue={{ games }}>
+      <InventoryListsProvider overrideValue={{ inventoryLists: emptyInventoryLists, inventoryListLoadingState: 'done' }}>
+        <InventoryPage />
+      </InventoryListsProvider>
+    </GamesProvider>
+  </AppProvider>
+)
+
+/*
+ *
+ * When the API data is loading
+ *
+ */
+
+export const Loading = () => {
+  const inventoryLists = allInventoryLists.filter(list => list.game_id === games[0].id)
+
+  return(
+    <AppProvider overrideValue={{ profileData, token }}>
+      <GamesProvider overrideValue={{ games }}>
+        <InventoryListsProvider overrideValue={{ inventoryLists, inventoryListLoadingState: 'loading' }}>
+          <InventoryPage />
+        </InventoryListsProvider>
+      </GamesProvider>
+    </AppProvider>
+  )
+}
+
+/*
+ *
+ * When there is an error with the API response
+ *
+ */
+
+export const ErrorState = () => (
+  <AppProvider overrideValue={{ profileData, token }}>
+    <GamesProvider overrideValue={{ games }}>
+      <InventoryListsProvider>
+        <InventoryPage />
+      </InventoryListsProvider>
+    </GamesProvider>
+  </AppProvider>
+)
+
+ErrorState.parameters = {
+  msw: [
+    rest.get(`${backendBaseUri}/inventory_lists`, (req, res, ctx) => {
+      return res(
+        ctx.status(500)
+      )
     })
   ]
 }
