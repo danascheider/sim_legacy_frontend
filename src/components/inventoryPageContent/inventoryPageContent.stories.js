23bee4bc2c10c0d8c4756801fe9f498227a4c8a9
Dana Scheider <dana.scheider@envato.com>
2021-08-16 19:25:19 +1000

    Add handlers to stories for new POST requests

--- src/components/inventoryPageContent/inventoryPageContent.stories.js
+++ src/components/inventoryPageContent/inventoryPageContent.stories.js
@@ -66,6 +66,78 @@ HappyPath.parameters = {
           ctx.json(newAggregateList)
         )
       }
+    }),
+    // This request adds an inventory list item to the inventory list requested, if that inventory
+    // list exists and belongs to the authenticated user. For the purposes of Storybook, we
+    // assume the user is authenticated and the `allInventoryLists` array represents all their
+    // inventory lists for all their games.
+    rest.post(`${backendBaseUri}/inventory_lists/:listId/inventory_list_items`, (req, res, ctx) => {
+      // Find the inventory list the user wants to add the item to
+      const listId = parseInt(req.params.listId)
+      const regList = allInventoryLists.find(list => list.id === listId)
+
+      if (regList) {
+        // If the inventory list exists and the request params are valid, the API will
+        // do one of two things. If the params are valid and there is an existing item
+        // on the same list with a matching description, it will update the existing item,
+        // adding the quantity from the request params and concatenating notes values, if
+        // any, so they are separated by `' -- '`. Then the API will find the matching
+        // item on the aggregate list and update it as well.
+        //
+        // If the params are valid and there is no existing item with a matching
+        // description on the same list, the API will create a new item on that list. Then,
+        // it will check to see if there is already a matching item on the aggregate list.
+        // If there is a matching item on the aggregate list, that item will be updated as
+        // described above. If there is no matching item, a new one will be created with
+        // the `notes` and `description` values from the request.
+        const description = req.body.inventory_list_item.description
+        const quantity = req.body.inventory_list_item.quantity || '1'
+
+        // Description and quantity are both required and neither can be blank. The
+        // quantity must be an integer as well. If the quantity is a decimal/float value
+        // greater than 1, it will be truncated and treated as an integer. If the
+        // quantity is non-numeric or less than 1, it is invalid.
+        if (description && quantity && (typeof quantity === 'number' || quantity.match(/[1-9]+(\.\d+)?/))) {
+          const regListItem = regList.list_items.find(item => item.description.toLowerCase() === description.toLowerCase())
+          const notes = req.body.inventory_list_item.notes
+
+          const aggregateList = findAggregateList(allInventoryLists, regList.game_id)
+          const aggregateListItem = aggregateList.list_items.find(item => item.description.toLowerCase() === description.toLowerCase())
+
+          if (regListItem) adjustListItem(regListItem, parseInt(quantity), regListItem.notes, notes)
+          if (aggregateListItem) adjustListItem(aggregateListItem, parseInt(quantity), aggregateListItem.notes, notes)
+
+          const defaultRegListItem = { id: Math.floor(Math.random() * 10000), list_id: regList.id, description, quantity }
+          const defaultAggListItem = { id: Math.floor(Math.random() * 10000), list_id: aggregateList.id, description, quantity }
+
+          // The API will return the updated aggregate list item along with the created or
+          // updated list item from the regular list with a 200 status.
+          const returnData = [aggregateListItem || defaultAggListItem, regListItem || defaultRegListItem]
+
+          return res(
+            ctx.status(200),
+            ctx.json(returnData)
+          )
+        } else {
+          const errors = []
+          // Invalid values will result in a 422 error. This will approximate API
+          // behaviour and error messages when a value is invalid.
+          if (!quantity) errors.push('Quantity is required')
+          if (quantity && !quantity.match(/[1-9]+(\.\d+)?/)) errors.push('Quantity must be a number')
+          // There are no validations on description format, it's just required to be there.
+          if (!description) errors.push('Description is required')
+
+          return res(
+            ctx.status(422),
+            ctx.json({ errors })
+          )
+        }
+      } else {
+        // If the inventory list is not found, the API will return a 404.
+        return res(
+          ctx.status(404)
+        )
+      }
     })
   ]
 }
